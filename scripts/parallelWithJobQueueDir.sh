5c52132a13667eb289a47e3e0486205af0a6592d
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-02-28 11:56:56 -0500

    Update parallelWithJobQueueDir.sh
    - Add function to reset test file set
    
    - Add reset_n_test_echo_scripts function to reset files in jobqueue dir (for testing)

--- scripts/parallelWithJobQueueDir.sh
+++ scripts/parallelWithJobQueueDir.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# fields
 jobQueuePath="../jobqueue"
 
-# DEBUG
+# DEBUG instrumentation
 echo "" > test-output
+reset_n_test_echo_scripts() {
+    find $jobQueuePath -type f -exec rm {} \;
+	echo "KILL SIGNAL RECIVED - resetting $1 test echo scripts"
+	seq 1 $1 | parallel "touch $jobQueuePath/{}"
+	seq 1 $1 | parallel "echo echo {} > $jobQueuePath/{}"
+	exit 0
+}
+
+
+# DEBUG traps
+trap "reset_n_test_echo_scripts 30" SIGTERM SIGINT
 
 while [ true ]
 do
 	files=$(ls $jobQueuePath)
-	paths=$(echo $files | xargs -I{} -d " " echo "$jobQueuePath/{}")
+	paths=$(echo -n $files | xargs --no-run-if-empty -I{} -d " " echo "$jobQueuePath/{}")
 
 	# for file in $files
 	# do
@@ -22,9 +34,10 @@ do
 	# parallel -d " " --no-run-if-empty echo >> test-output ::: $paths
 
 	# feed parallel the paths of the files in the job queue so that it executes them, and then remove each file that ran successfully
-	echo $paths | parallel -d " " --no-run-if-empty 'sh {} >> test-output && rm {} || echo "failed to process file {}"'
+	echo $paths | parallel -d " " --no-run-if-empty \
+					'sh {} >> test-output && rm {} || echo "failed to process file {}"'
 
-	sleep 5 # seconds
+	sleep 2 # seconds
 done
 
 # TODO Have parallel process individual job (with another parallel process?) (based on filename?)
