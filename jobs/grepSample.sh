da58fa693a0479f4934794098081b6e6c3374554
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-10 22:24:02 -0500

    [TEST] Add sample job scripts to the jobs dir
    - sedSample.sh to run sed a few times to replace g's, each sed takes a few seconds to run on the 1GB random text file
    
    - grepSample.sh to run grep a few times on 1GB random text file, it is very quick.

---
+++ jobs/grepSample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+grep g ../input/sample.txt > /dev/null
+echo "grepped 1 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 2 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 3 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 4 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 5 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 6 time"
+grep g ../input/sample.txt > /dev/null
+echo "grepped 7 times"
+echo "Done!"
---
+++ jobs/sedSample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 1 time"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 2 times"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 3 times"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 4 times"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 5 times"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 6 times"
+sed -n 's/g/fox/p' ../input/sample.txt > ../input/g-to-fox-sample.txt
+echo "sedded 7 times"
