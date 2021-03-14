a5e5badee498ec698ee1e403212daa6dae2e8d83
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-14 18:47:31 -0400

    Update parallelWithEchoedJobCommands.sh
    Add archive folder for successfully processed job scripts that echo commands

--- scripts/parallelWithEchoedJobCommands.sh
+++ scripts/parallelWithEchoedJobCommands.sh
@@ -9,6 +9,9 @@ echo "" > test-output
 # DEBUG traps
 # trap "reset_n_test_echo_scripts 30;" SIGTERM SIGINT
 
+# Create directories
+mkdir -p ../archive ../error ../output
+
 while [ true ]
 do
 	files=$(ls $jobQueuePath)
@@ -25,14 +28,14 @@ do
 	# echo paths of files that will eventually be executed
 	# parallel -d " " --no-run-if-empty echo >> test-output ::: $paths
 
-	echo $paths
+	echo $paths > test-command-output
 	# Have parallel process commands sent as output from job scripts 
 	# (with another parallel process) 
 	# (removing each file that runs successfully)
 	echo -n $paths | parallel -j2 -d " " --no-run-if-empty \
-					'bash {} | parallel -I___ -j6 bash -c ___ >> test-command-output && rm {} || echo "failed to process file {}"'
+					'bash {} | parallel -I___ -j6 "bash -c ___ >> test-command-output" && mv {} ../archive' 
 
-	sleep 10 # seconds
+	sleep 1 # seconds
 done
 
 # TODO 
