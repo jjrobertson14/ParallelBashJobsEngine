2d3b7ed37a9d516b8080da2874be65b1c800641e
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-20 10:22:44 -0400

    [parallelWithEchoedJobCommands.sh] Update parallelWithEchoedJobCommands.sh  to output commands that have error to a command-error text file.
    - Also did some clean up

--- scripts/parallelWithEchoedJobCommands.sh
+++ scripts/parallelWithEchoedJobCommands.sh
@@ -4,14 +4,13 @@
 jobQueuePath="../jobqueue-for-jobs-that-echo-commands"
 
 # DEBUG instrumentation
-echo "" > test-output
-rm -f ../error/* ../archive/*
+# rm -f ../error/* ../archive/* 
 
 # DEBUG traps
 # trap "reset_n_test_echo_scripts 30;" SIGTERM SIGINT
 
 # Create directories
-mkdir -p ../archive ../error ../output
+mkdir -p ../archive ../error ../output ../input
 
 while [ true ]
 do
@@ -23,18 +22,13 @@ do
 		# echo $file && rm ./jobqueue/$file || echo "failed to process file $(file)"
 	# done
 
-	# echo content of files
-	# echo $files >> test-output 
-	
-	# echo paths of files that will eventually be executed
-	# parallel -d " " --no-run-if-empty echo >> test-output ::: $paths
-
-	echo $paths > test-command-output
+	echo $paths > command-output
 	# Have parallel process commands sent as output from job scripts 
 	# (with another parallel process) 
 	# (removing each file that runs successfully)
 	echo -n $paths | parallel -j2 -d " " --no-run-if-empty \
-					'bash {} || mv {} ../error | parallel -I___ -j6 "bash -c ___ >> test-command-output || mv {} ../error"' 
+					# 'bash {} || mv {} ../error | parallel -I___ -j6 "bash -c ___ >> command-output || echo ___ >> command-error"' 
+					'bash {} || mv {} ../error | parallel -I___ -j6 "bash -c ___ >> command-output || mv {} ../error"' 
 	
 	for path in $paths
 	do
@@ -46,5 +40,3 @@ do
 
 	sleep 1 # seconds
 done
-
-# TODO 
