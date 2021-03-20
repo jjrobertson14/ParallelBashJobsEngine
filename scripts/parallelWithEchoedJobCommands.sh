998435cb7da6fab42dcdcc1ad7592ef9ea1fc0e9
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-20 14:47:12 -0400

    [feature] - add date-timestamp to  job script files that fail to process that are moved to the error directory [bug fix] - set execute flag on job script files put in job queue

--- scripts/parallelWithEchoedJobCommands.sh
+++ scripts/parallelWithEchoedJobCommands.sh
@@ -2,6 +2,7 @@
 
 # fields
 jobQueuePath="../jobqueue-for-jobs-that-echo-commands"
+dateStampFmt="+%Y%m%d-%H:%M:%S.%s" # use like `date $dateStampFmt`
 
 # DEBUG instrumentation
 rm -f test-output
@@ -27,13 +28,15 @@ do
 		echo $jobFilePaths >> test-output
 	fi
 
+	echo -n $jobFilePaths |xargs -I{} chmod +x {}
+
 	# Have parallel process commands sent as output from job scripts 
 	# (with another parallel process) 
 	# (moving each file that runs successfully to archive dir)
 		# (moving each file fails to run successfully to error dir)
 		# (writing each command echoed by a job script file that fails to run successfully to command-error file)
 	echo -n $jobFilePaths | parallel -j2 -d " " --no-run-if-empty \
-		'bash {} || echo {} ../error/$(date +%s%N) | parallel -I___ -j6 "bash -c ___ >> command-output || echo ___ >> ../error/command-error"'
+		'bash {} || mv {} ../error/$(echo {} |cut -d"/" -f3 |cut -d"." -f1)_$(date +%Y%m%d-%H:%M:%S.%s) | parallel -I___ -j6 "bash -c ___ >> command-output || echo ___ >> ../error/command-error"'
 		# 'bash {} || echo {} ../error/$(date +%s%N)_ | parallel -I___ -j6 "bash -c ___ >> command-output || mv {} ../error"' 
 	
 	# for jobfilename in $jobFileNames
