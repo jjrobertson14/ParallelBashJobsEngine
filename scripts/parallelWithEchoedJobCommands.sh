058b5503b7efc94256c39bf7998c27a4f27e8783
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-20 11:21:55 -0400

    [features] - Add handling of failing to set variables and skipping code if failure occurrs - Add moving job files that fail to process to error directory with timestamp - Add writing commands echoed by job script files to error file (with timestamp)

--- scripts/parallelWithEchoedJobCommands.sh
+++ scripts/parallelWithEchoedJobCommands.sh
@@ -4,6 +4,7 @@
 jobQueuePath="../jobqueue-for-jobs-that-echo-commands"
 
 # DEBUG instrumentation
+rm test-output
 # rm -f ../error/* ../archive/* 
 
 # DEBUG traps
@@ -14,21 +15,26 @@ mkdir -p ../archive ../error ../output ../input
 
 while [ true ]
 do
-	files=$(ls $jobQueuePath)
-	jobFileNames=$(echo -n $files | xargs --no-run-if-empty -I{} -d " " echo "{}")
+	jobFileNames=$(ls $jobQueuePath) \
+					|| echo "failed to create var jobFileName" >> test-output
+	jobFilePaths=$(echo -n $jobFileNames | xargs --no-run-if-empty -I{} -d " " echo "$jobQueuePath/{}") \
+					|| echo "failed to create var jobFilePaths" >> test-output
+	if [ -z $jobFileNames ] || [ -z $jobFilePaths ]
+	then
+		continue
+	else
+		echo $jobFileNames >> test-output
+		echo $jobFilePaths >> test-output
+	fi
 
-	# for file in $files
-	# do
-		# echo $file && rm ./jobqueue/$file || echo "failed to process file $(file)"
-	# done
-
-	echo $jobFileNames > command-output
 	# Have parallel process commands sent as output from job scripts 
 	# (with another parallel process) 
-	# (removing each file that runs successfully)
-	# echo -n $jobFileNames | parallel -j2 -d " " --no-run-if-empty \
-					# 'bash {} || mv {} ../error | parallel -I___ -j6 "bash -c ___ >> command-output || echo ___ >> command-error"' 
-					# 'bash {} || echo {} ../error/$(date +%s%N)_ | parallel -I___ -j6 "bash -c ___ >> command-output || mv {} ../error"' 
+	# (moving each file that runs successfully to archive dir)
+		# (moving each file fails to run successfully to error dir)
+		# (writing each command echoed by a job script file that fails to run successfully to command-error file)
+	echo -n $jobFilePaths | parallel -j2 -d " " --no-run-if-empty \
+		'bash {} || mv {} ../error/$(date +%s%N)_$(echo {} | cut -d"/" -f2) | parallel -I___ -j6 "bash -c ___ >> command-output || echo ___ >> command-error"' 
+		# 'bash {} || echo {} ../error/$(date +%s%N)_ | parallel -I___ -j6 "bash -c ___ >> command-output || mv {} ../error"' 
 	
 	for jobfilename in $jobFileNames
 	do
