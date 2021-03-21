fd6d08548f7c9068c5159bc800bb37050586e56e
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-21 13:44:35 -0400

    [fixup] fix up succeeds_sedSampleOutputCommands.sh by adding _-_-_COMMAND- tokens so that the echoed commands are interpreted as commands by the job engine

--- jobs/succeeds_sedSampleOutputCommands.sh
+++ jobs/succeeds_sedSampleOutputCommands.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-1.txt && echo ran sed 1 time"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-2.txt && echo ran sed 2 times"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-3.txt && echo ran sed 3 times"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-4.txt && echo ran sed 4 times"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-5.txt && echo ran sed 5 times"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-6.txt && echo ran sed 6 times"
-echo "sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-7.txt && echo ran sed 7 times"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"
-# echo "sed -n 's/g/fox/p' ../input/sample.txt"success
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-1.txt && echo ran sed 1 time"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-2.txt && echo ran sed 2 times"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-3.txt && echo ran sed 3 times"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-4.txt && echo ran sed 4 times"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-5.txt && echo ran sed 5 times"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-6.txt && echo ran sed 6 times"
+echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt > ../output/g-to-fox-sample-7.txt && echo ran sed 7 times"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"
+# echo "_-_-_COMMAND-sed -n 's/g/fox/p' ../input/sample.txt"success
