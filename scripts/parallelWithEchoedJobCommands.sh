e3665f3aec5387affc9a46ce83a5d542f9095dbd
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-03-21 11:45:41 -0400

    [feature] add only running lines of output of job script files that start with a token "_-_-_COMMAND-"
    - Also add creating used directories

--- scripts/parallelWithEchoedJobCommands.sh
+++ scripts/parallelWithEchoedJobCommands.sh
@@ -13,7 +13,7 @@ rm -f test-output test-error
 # trap "reset_n_test_echo_scripts 30;" SIGTERM SIGINT
 
 # Create directories
-mkdir -p ../archive ../error ../output ../input
+mkdir -p ../archive ../error ../output ../input ../jobqueue ../jobqueue-for-jobs-that-echo-commands
 
 while [ true ]
 do
@@ -36,15 +36,19 @@ do
 		# Run job script and take output as commands to run via parallel
 		# (moving each file fails to run successfully to error dir)
 	cGetCommandsFromJobScript='( bash {} || mv {} ../error/$(echo {} |cut -d"/" -f3 |cut -d"." -f1)_$(date +%Y%m%d-%H:%M:%S.%s) )'
+		# In order to allow only specified output of job scripts to be ran as command, 
+			# have token "_-_-_COMMAND-" be at start of output strings client wants to run as commands
+		# (To do this, grep to select text after token "_-_-_COMMAND-" if it appears at start of piped in string)
+	cGrepActualCommandsByToken='( grep -oP "(?<=^_-_-_COMMAND-).*" )'
 		# Have parallel process commands sent as output from job scripts 
 		# (writing each command echoed by a job script file that fails to run successfully to command-error file)
-	cRunJobCommands='parallel -I___ -j6 "bash -c ___ >> command-output || echo $(echo {} |cut -d"/" -f3 |cut -d"." -f1)_$(date +%Y%m%d-%H:%M:%S.%s) ___ >> ../error/command-error"'
+	cRunJobCommands='( parallel -I___ -j6 "bash -c ___ >> command-output || echo $(echo {} |cut -d"/" -f3 |cut -d"." -f1)_$(date +%Y%m%d-%H:%M:%S.%s) ___ >> ../error/command-error" )'
 	# [ END COMMAND STRING COMPONENTS ]
 	
 	# [RUN PARALLEL] 
 	# Pass each job to parallel process that runs subcommands
 	echo -n $jobFilePaths | parallel -j2 -d " " --no-run-if-empty \
-		"${cGetCommandsFromJobScript} | ${cRunJobCommands}"
+		"${cGetCommandsFromJobScript} | ${cGrepActualCommandsByToken} | ${cRunJobCommands}"
 	
 	# [ARCHIVE]
 	for jobfilename in $jobFileNames
