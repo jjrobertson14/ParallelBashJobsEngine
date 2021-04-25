e040cc11b83147a587ce07f4b81a81db3d34cdbb
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-04-25 15:10:53 -0400

    [#3] getting init script working

--- initialize_ParallelBashJobsEngine.sh
+++ initialize_ParallelBashJobsEngine.sh
@@ -1,10 +1,15 @@
-# TODO get pbjengine installed in /opt/pbjengine (with pbjengine/engine/logs/{pid,startpid})
-# TODO get this directory to be created... /etc/pbjengined
+# TODO get pbjengine installed in /opt/pbjengine 
+    # Create directories: bin
+    # Copy files these files to it: 
+        # cp -a pbjengined_initd /etc/init.d/pbjengined
+        # cp -a scripts/parallelWithEchoedJobCommands.sh /opt/pbjengine/bin
+        # 
 
+# TODO get this directory to be created?... /etc/pbjengined
 # TODO create /etc/pbjengined.conf
 
-cp pbjengined_initd /etc/init.d/pbjengined
 
+cp pbjengined_initd /etc/init.d/pbjengined
 # After creating the file run sudo update-rc.d myservice defaults to install your 
 # service (here referred as myservice). refer to update-rec.d, Then you can start 
 # and stop your service using sudo service myservice start
@@ -12,3 +17,4 @@ cp pbjengined_initd /etc/init.d/pbjengined
 # [https://askubuntu.com/questions/1261213/how-to-write-an-init-script-that-will-execute-an-existing-start-script-in-ubuntu]
 # TODO update 'myservice' text here
 sudo update-rc.d myservice defaults
+
